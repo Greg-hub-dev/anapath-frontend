3121b3b3a3797cda4141953b9edc84eaaf089c68
Greg-hub-dev <g.puscian@gmail.com>
2025-03-27 09:16:56 +0100

    Update cellAI_2.py

--- cellAI_2.py
+++ cellAI_2.py
@@ -200,11 +200,11 @@ if navigation == "Home":
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 4"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 5"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 6"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 7"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 8"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -365,11 +365,11 @@ elif navigation == "Analyse et Résultats":
         </div>
             """, unsafe_allow_html=True)
 
-         # Affichage de l'image téléchargée - A modifier greg
+         # Affichage de l'image téléchargée
         if uploaded_file is not None:
             image = Image.open(uploaded_file)
             st.image(image, caption="Image histologique téléchargée", use_container_width=True)
-    
+
                 # Informations sur l'image
             st.markdown(f"**Dimensions:** {image.width} × {image.height} pixels")
             st.markdown(f"**Format:** {uploaded_file.type}")
